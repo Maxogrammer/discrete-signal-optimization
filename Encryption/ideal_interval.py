846b3f2ff79f6d3cba561c70904854aca6a560e4
Maxogrammer <75534302+Maxogrammer@users.noreply.github.com>
2024-11-05 11:29:55 +0300

    Добавлены нахождение общего идеального интервала, консольное приложение для записи новых tau, ЦНОД больше не учитывает кратность

--- Encryption/ideal_interval.py
+++ Encryption/ideal_interval.py
@@ -1,21 +1,34 @@
-from json import load, dump
-from os import path as ospath
+from json import load
+from os import path as ospath, listdir
 
 
-def find_tau(phone_number: int, action_type: str) -> None:
+def prime_divisors(n: int) -> list:
 	'''
-	Берёт данные 1 телефона за все циклы из файла Phone[i]chr.json,
-	по специальному алгоритму находит tau и записывает его в файл
-	Cipher/Phone[i].json.
+	Находит все простые делители числа, учитывая кратные.
 	'''
 
-	# Алгоритм — умный НОД (УНОД)
+	i = 2
+	factors = []
+	while i * i <= n:
+		if n % i:
+			i += 1
+		else:
+			n //= i
+			factors.append(i)
+	if n > 1:
+		factors.append(n)
 
-	# Путь к данным
+	return factors
+
+def get_nums_from_file(phone_number: int, action_type: str) -> list:
+	'''
+	Отбирает данные нужного типа деятельности из
+	одного файла Cipher/Phone[i]chr.json.
+	'''
+
 	current_directory = ospath.dirname(__file__)
 	file_path = ospath.join(current_directory, '..', 'Data', 'PreparedExp', f'Phone{phone_number}chr.json')
 
-	# Отбор данных нужного типа деятельности
 	with open(file_path, 'r') as file:
 		data = load(file)
 
@@ -25,30 +38,20 @@ def find_tau(phone_number: int, action_type: str) -> None:
 			for j in range(1, len(cycle)):
 				pair = cycle[j]
 				numbers.append(pair[0]) if (pair[1] == action_type) else None
+	
+	return numbers
 
-	# Нахождение всех простых делителей
-	def prime_divisors(n):
-		i = 2
-		factors = []
-		while i * i <= n:
-			if n % i:
-				i += 1
-			else:
-				n //= i
-				factors.append(i)
-		if n > 1:
-			factors.append(n)
-
-		return factors
+def calc_tau(numbers: list) -> int:
+	'''
+	ЦНОД (Центрированный НОД) — считает tau.
+	'''
 
 	# Подсчёт количества всех простых делителей по отдельности
-	n = len(numbers)
-
 	data = {}
 	total = 0
 
 	for num in numbers:
-		factors = prime_divisors(num)
+		factors = list(set(prime_divisors(num))) # Не учитываем кратные множители
 
 		for j in factors:
 			total += 1
@@ -66,15 +69,41 @@ def find_tau(phone_number: int, action_type: str) -> None:
 	# Идеальный интервал — сумма / суммарное кол-во всех простых делителей. По сути, центр масс.
 	tau = sum_tmp // total
 
-	# Сохранение tau
-	file_path = ospath.join(current_directory, '..', 'Encryption', 'Cipher', f'Phone{phone_number}.json')
+	return tau
 
-	with open(file_path, "r") as cipher:
-		data = load(cipher)
 
-	data[action_type]["tau"] = tau
+def find_tau(phone_number: int, action_type: str) -> int:
+	'''
+	Берёт данные 1 телефона за все циклы из файла Phone[i]chr.json,
+	по специальному алгоритму находит tau.
+	'''
 
-	with open(file_path, "w") as cipher:
-		dump(data, cipher, indent=4)
+	# Сбор данных
+	numbers = get_nums_from_file(phone_number, action_type)
 
-def find_tau_common(): pass
+	# Расчёт tau
+	tau = calc_tau(numbers)
+
+	return tau
+
+def find_tau_common(action_type: str) -> int:
+	'''
+	Берёт данные всех телефонов за все циклы из файлов Phone[i]chr.json,
+	по специальному алгоритму находит tau.
+	'''
+
+	# Считает количество файлов
+	current_directory = ospath.dirname(__file__)
+	DIR = str(ospath.join(current_directory, '..', 'Data', 'PreparedExp'))
+
+	files_num = len([name for name in listdir(DIR) if ospath.isfile(ospath.join(DIR, name))])
+
+	# Сбор данных
+	numbers = []
+	for i in range(1, files_num + 1):
+		numbers += get_nums_from_file(i, action_type)
+
+	# Расчёт tau
+	tau = calc_tau(numbers)
+
+	return tau
