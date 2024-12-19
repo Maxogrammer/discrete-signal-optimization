1906a8de8cd640afb91a37c39d28a6fb895ffd3e
Maxogrammer <75534302+Maxogrammer@users.noreply.github.com>
2024-12-19 17:25:04 +0300

    Small refactoring

--- Application/visualization.py
+++ Application/visualization.py
@@ -1,19 +1,34 @@
 import tkinter as tk
 from tkinter import filedialog, messagebox
 import json
+from sys import path as syspath
 from os import path as ospath
+
 import matplotlib.pyplot as plt
-import re
-import prepare1, encrypter1
+
+syspath.append('./')
+import Data.prepare as prepare1, Encryption.encrypter as encrypter1
 
 window = tk.Tk()
 window.title("График зависимости заряда батареи от времени")
 window.geometry("800x600")
+
 text_for_terminal = "___"
-text_for_terminal1 = "___"
+text_for_terminal1 = "VVV Введите номер строки VVV"
 text_for_txt = "___"
+
+usage_power = {
+    '@': 0.0675,
+    '&': 0.0676,
+    'O': 0.0674,
+    'I': 0.0674,    
+}
+
 terminal = tk.Text(window)
 terminal.place_configure(x=10, y=40, width=700, height=200)
+
+aboba_terminator = tk.Label(window, text=text_for_terminal1)
+aboba_terminator.place_configure(x=10, y=270)
 
 writen1 = []
 
@@ -34,83 +49,51 @@ def load_json_file():
         messagebox.showerror("Ошибка", f"Не удалось загрузить файл: {e}")
         return None
 
-
 def plot_graph():
     """Функция для построения графика на основе данных."""
-    line1 = terminal.get("1.0", tk.END)
 
-    char_counts = {}
-    percent_counts = {}
+    ciphered_string = txt.get("1.0", tk.END)
 
-    for char in line1:
-        if char in char_counts:
-            char_counts[char] += 1
-        else:
-            char_counts[char] = 1
+    current_directory = ospath.dirname(__file__)
+    data_path = ospath.join(current_directory, ".." , 'Data', 'PreparedExp', 'Phone1.json')
+    
+    with open(data_path, "r") as file:
+        full_data = json.load(file)[int(true_terminator.get("1.0", tk.END)) - 1]
+        perc0, perc_end, data = full_data[0][0], full_data[0][1], full_data[1:]
 
-    diff = 100
+    current_directory = ospath.dirname(__file__)
+    data_path = ospath.join(current_directory, ".." , 'Encryption', 'tau_common.json')
+    
+    with open(data_path, "r") as file:
+        tau_dict = json.load(file)
 
-    first = 0
-    sec = 0
-    third = 0
-    fourth = 0
+    y_axis = [perc0]
+    x_axis = [0]
 
-    arr = []
-    sum_value = 0
+    for i in range(len(ciphered_string) - 1):
+        char = ciphered_string[i]
 
-    for key, value in char_counts.items():
-        sum_value += value
-        arr.append(sum_value)
+        x_axis.append(x_axis[-1] + tau_dict[char])
+        y_axis.append(y_axis[-1] - usage_power[char]*tau_dict[char])
 
-    for value in arr:
-        print(value)
+    y_orig = [perc0]
+    x_orig = [0]
 
-    for char, count in char_counts.items():
-        if char == '@':
-            percent_counts[char] = count * 0.675
-        elif char == "O":
-            percent_counts[char] = count * 0.0674
-        elif char == "1":
-            percent_counts[char] = count * 0.06974
-        elif char == "&":
-            percent_counts[char] = count * 0.676
+    for i in range(len(data)):
+        x_orig.append(x_orig[-1] + data[i][0])
+        y_orig.append(y_orig[-1] - usage_power[char]*data[i][0])
 
-    for char, count in percent_counts.items():
-        if char == '@':
-            diff -= count
-            first = diff
+    plt.figure(figsize=(10, 6))
+    plt.plot(x_axis, y_axis, marker='o', linestyle='-', color='b', label="С шифровкой", linewidth=0.5, markersize=3)
+    plt.plot(x_orig, y_orig, marker='|', linestyle='dashed', color='g', label="Оригинал", linewidth=0.5, markersize=10)
+    plt.plot([0, x_orig[-1]], [perc0, perc_end], marker=7, color='r', label="Начальная и конечная точки", linewidth=0, markersize=10)
 
-        elif char == "O":
-            diff -= count
-            sec = diff
-
-        elif char == "1":
-            diff -= count
-            third = diff
-
-        elif char == "&":
-            diff -= count
-            fourth = diff
-
-    if line1 != "":
-        x_values = arr[:4]  # Используем массив arr для значений x
-        y_values = [first, sec, third, fourt]
-
-        plt.figure(figsize=(8, 6))
-        plt.plot(x_values, y_values, marker='o', linestyle='-', color='b', label="Graph")
-        plt.xlabel("Time (calculated from char_counts)")
-        plt.ylabel("Values (first, sec, third, fourth)")
-        plt.title("Graph of Calculated Values vs Time")
-        plt.legend()
-        plt.grid(True)
-        plt.show()
-
-def format_input(data_str):
-    formatted = data_str.replace('{', '[').replace('}', ']')
-    formatted = re.sub(r'(\d+)\s+([A-Za-z&@])', r'\1, "\2"', formatted)
-    formatted = re.sub(r'(\d+)\s+(\d+)', r'\1, \2', formatted)
-    formatted = formatted.replace('] [', '], [')
-    return formatted
+    plt.xlabel("Время в течение цикла (мин)")
+    plt.ylabel("Процент заряда телефона")
+    plt.title("Зависимость заряда телефона от времени")
+    plt.legend()
+    plt.grid(True)
+    plt.show()
 
 def load_and_plot():
     """Загружает данные и строит график."""
@@ -122,9 +105,9 @@ def load_and_plot():
 
 def encr():
     global writen1
-    #encrypter1.encrypter()
     txt.delete('1.0', tk.END)
-    text_for_txt = encrypter1.encrypter()
+    text_for_txt = encrypter1.encrypter(int(true_terminator.get("1.0", tk.END)) - 1)
+
     txt.insert(tk.END, text_for_txt)
 
 ###########################
@@ -136,7 +119,7 @@ def load_txt_file():
     except Exception as e:
         messagebox.showerror("Ошибка", f"Не удалось загрузить файл: {e}")
         return None
-   
+
 def load_for_prepare():
     try:
         file_path = filedialog.askopenfilename(title="Выберите TXT файл")
@@ -146,12 +129,21 @@ def load_for_prepare():
         pass
 
 ############################
+
 def print1():
     terminal.delete("1.0", tk.END)
-    text_for_terminal = load_json_file()
+
+    current_directory = ospath.dirname(__file__)
+    data_path = ospath.join(current_directory, ".." , 'Data', 'PreparedExp', 'Phone1.json')
+    
+    with open(data_path, "r") as file:
+        text_for_terminal = json.load(file)
+
     terminal.insert(tk.END, text_for_terminal)
+
 def clear():
     terminal.delete("1.0", tk.END)
+
 ############################
 
 clear_button = tk.Button(window, text="clear", command=clear, relief=tk.FLAT, bg="lightgrey")
@@ -171,5 +163,8 @@ graph_paint.place_configure(x=10, y=420)
 
 encrypter_button = tk.Button(window, text="encrypt", command=encr, relief=tk.FLAT, bg="lightgrey")
 encrypter_button.place_configure(x=410, y=10)
+
+true_terminator = tk.Text(window)
+true_terminator.place_configure(x=10, y=300, width=100, height=30)
 
 window.mainloop()
